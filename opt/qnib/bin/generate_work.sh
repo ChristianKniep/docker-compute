3b98f23a01ed2ce1b59598ae07763a94a8ad2293
Christian Kniep <christian@qnib.org>
2015-05-01 13:49:01 +0200

    new generate work script

--- opt/qnib/bin/generate_work.sh
+++ opt/qnib/bin/generate_work.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 users=(alice bob carol dave eve john jane)
 nodes=$(scontrol show partition|egrep -o "TotalNodes=[0-9]+"|head -n1|egrep -o "[0-9]+")
-for x in {1..10};do
+sjobs=(ping_pong gemm)
+for x in $(seq 1 ${1-5});do
     num=$(shuf -i 2-${nodes} -n 1)
+    echo "num: ${num}"
     user=${users[$[ $RANDOM % ${#users[@]} ]]}
-    echo ">> su -c 'sbatch -N${num} /opt/qnib/jobscripts/ping_pong.sh' ${user}"
-    su -c "sbatch -N${num} /opt/qnib/jobscripts/ping_pong.sh" ${user}
+    if [ "X${2}" == "X" ];then
+        job=${sjobs[$[ $RANDOM % ${#sjobs[@]} ]]}
+    else
+        job=${2}
+    fi
+    if [ ${job} == "gemm" ];then
+        exp=$(shuf -i 1-$(echo "sqrt(${nodes})"|bc) -n 1)
+        echo "exp: ${exp}"
+        num=$(echo "2^${exp}"|bc)
+        echo "num: ${num}"
+        echo ">> su -c 'sbatch -N${num} /opt/qnib/jobscripts/gemm.sh' ${user}"
+        #su -c "sbatch -N${num} /opt/qnib/jobscripts/gemm.sh" ${user}
+    else
+        echo ">> su -c 'sbatch -N${num} /opt/qnib/jobscripts/ping_pong.sh' ${user}"
+        su -c "sbatch -N${num} /opt/qnib/jobscripts/ping_pong.sh" ${user}
+    fi
 done
