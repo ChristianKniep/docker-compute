e9806f710c68a58f8255de2a56b68f4f724f29dd
Christian Kniep <christian@qnib.org>
2015-05-02 10:47:00 +0200

    make failing more fun

--- opt/qnib/jobscripts/ping_pong.sh
+++ opt/qnib/jobscripts/ping_pong.sh
@@ -6,16 +6,26 @@
 #SBATCH --ntasks-per-node=1
 
 ERR_NODE=${1-noerrornode}
+if [ ${ERR_NODE} != "noerrornode" ];then
+    logger --tag slurm_${SLURM_JOBID} "Error Node: ${ERR_NODE}"
+    sleep 2
+fi
 mkdir -p /scratch/${SLURM_JOBID}/
 cd /scratch/${SLURM_JOBID}/
 DOMAIN=node.consul
 BW_LIMIT=3000
 NODES=$(python -c "from ClusterShell.NodeSet import NodeSet;print ' '.join(NodeSet('${SLURM_NODELIST}'))")
+if [ $(echo ${NODES}|grep -c ${ERR_NODE}) -ne 0 ];then
+    # let job fail after 15sec
+    sleep 15
+    logger -t slurm_${JOBID}  "Job failed (tell you a secret: due to error node trigger)"
+    exit 42
+fi
+if [ ${SLURMD_NODENAME} == ${ERR_NODE} ];then
+    logger -t slurm_${JOBID}  "Job failed (tell you a secret: due to error node trigger)"
+    exit 42
+fi
 for node in ${NODES};do
-    if [ ${node} == ${ERR_NODE} ];then
-        logger -t slurm_${JOBID}  "Job failed (tell you a secret: due to error node trigger)"
-        exit 42
-    fi
     if [ ${node} != ${SLURMD_NODENAME} ];then
         logger --tag slurm_${SLURM_JOBID} "fallocate -l 60M /tmp/test_${SLURM_JOBID}.dd"
         srun --exclusive -n1 fallocate -l 60M /tmp/test_${SLURM_JOBID}.dd
